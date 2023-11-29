be2060035655ffac2be4c37744c08a8650babffa
Theodoros Kouzelis <theodoros.kouzelis@athenarc.gr>
2023-11-29 14:02:09 +0100

    latest

--- run_features_extraction.py
+++ run_features_extraction.py
@@ -130,139 +130,139 @@ def main():
         exp_config.config.prompt = ""
         exp_config.config.scale = 1.0
         
-    # seed = exp_config.config.seed 
+    seed = exp_config.config.seed 
 
-    for seed in range(500):
+    # for seed in range(500):
 
-        seed_everything(seed)
+    seed_everything(seed)
 
-        model = load_model_from_config(model_config, f"{opt.ckpt}")
+    model = load_model_from_config(model_config, f"{opt.ckpt}")
 
-        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-        model = model.to(device)
-        unet_model = model.model.diffusion_model
-        sampler = DDIMSampler(model)
-        save_feature_timesteps = exp_config.config.ddim_steps if exp_config.config.init_img == '' else exp_config.config.save_feature_timesteps
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = model.to(device)
+    unet_model = model.model.diffusion_model
+    sampler = DDIMSampler(model)
+    save_feature_timesteps = exp_config.config.ddim_steps if exp_config.config.init_img == '' else exp_config.config.save_feature_timesteps
 
-        outpath = f"{exp_path_root}/{exp_config.config.experiment_name}_{seed}"
+    outpath = f"{exp_path_root}/{exp_config.config.experiment_name}_{seed}"
 
-        callback_timesteps_to_save = [save_feature_timesteps]
-        if os.path.exists(outpath):
-            logging.warning("Experiment directory already exists, previously saved content will be overriden")
-            if exp_config.config.init_img != '':
-                with open(os.path.join(outpath, "args.json"), "r") as f:
-                    args = json.load(f)
-                callback_timesteps_to_save = args["save_feature_timesteps"] + callback_timesteps_to_save
+    callback_timesteps_to_save = [save_feature_timesteps]
+    if os.path.exists(outpath):
+        logging.warning("Experiment directory already exists, previously saved content will be overriden")
+        if exp_config.config.init_img != '':
+            with open(os.path.join(outpath, "args.json"), "r") as f:
+                args = json.load(f)
+            callback_timesteps_to_save = args["save_feature_timesteps"] + callback_timesteps_to_save
 
-        predicted_samples_path = os.path.join(outpath, "predicted_samples")
-        feature_maps_path = os.path.join(outpath, "feature_maps")
-        sample_path = os.path.join(outpath, "samples")
-        os.makedirs(outpath, exist_ok=True)
-        os.makedirs(predicted_samples_path, exist_ok=True)
-        os.makedirs(feature_maps_path, exist_ok=True)
-        os.makedirs(sample_path, exist_ok=True)
+    predicted_samples_path = os.path.join(outpath, "predicted_samples")
+    feature_maps_path = os.path.join(outpath, "feature_maps")
+    sample_path = os.path.join(outpath, "samples")
+    os.makedirs(outpath, exist_ok=True)
+    os.makedirs(predicted_samples_path, exist_ok=True)
+    os.makedirs(feature_maps_path, exist_ok=True)
+    os.makedirs(sample_path, exist_ok=True)
 
-        # save parse_args in experiment dir
-        with open(os.path.join(outpath, "args.json"), "w") as f:
-            args_to_save = OmegaConf.to_container(exp_config.config)
-            args_to_save["save_feature_timesteps"] = callback_timesteps_to_save
-            json.dump(args_to_save, f)
+    # save parse_args in experiment dir
+    with open(os.path.join(outpath, "args.json"), "w") as f:
+        args_to_save = OmegaConf.to_container(exp_config.config)
+        args_to_save["save_feature_timesteps"] = callback_timesteps_to_save
+        json.dump(args_to_save, f)
 
-        def save_sampled_img(x, i, save_path):
-            x_samples_ddim = model.decode_first_stage(x)
-            x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-            x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
-            x_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
-            x_sample = x_image_torch[0]
-            x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-            img = Image.fromarray(x_sample.astype(np.uint8))
-            img.save(os.path.join(save_path, f"{i}.png"))
+    def save_sampled_img(x, i, save_path):
+        x_samples_ddim = model.decode_first_stage(x)
+        x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+        x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+        x_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
+        x_sample = x_image_torch[0]
+        x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+        img = Image.fromarray(x_sample.astype(np.uint8))
+        img.save(os.path.join(save_path, f"{i}.png"))
 
-        def ddim_sampler_callback(pred_x0, xt, i):
-            save_feature_maps_callback(i)
-            save_sampled_img(pred_x0, i, predicted_samples_path)
+    def ddim_sampler_callback(pred_x0, xt, i):
+        save_feature_maps_callback(i)
+        save_sampled_img(pred_x0, i, predicted_samples_path)
 
-        def save_feature_maps(blocks, i, feature_type="input_block"):
-            block_idx = 0
-            for block in tqdm(blocks, desc="Saving input blocks feature maps"):
-                if not opt.save_all_features and block_idx < 4:
-                    block_idx += 1
-                    continue
-                if "ResBlock" in str(type(block[0])):
-                    if opt.save_all_features or block_idx == 4:
-                        save_feature_map(block[0].in_layers_features, f"{feature_type}_{block_idx}_in_layers_features_time_{i}")
-                        save_feature_map(block[0].out_layers_features, f"{feature_type}_{block_idx}_out_layers_features_time_{i}")
-                if len(block) > 1 and "SpatialTransformer" in str(type(block[1])):
-                    save_feature_map(block[1].transformer_blocks[0].attn1.k, f"{feature_type}_{block_idx}_self_attn_k_time_{i}")
-                    save_feature_map(block[1].transformer_blocks[0].attn1.q, f"{feature_type}_{block_idx}_self_attn_q_time_{i}")
+    def save_feature_maps(blocks, i, feature_type="input_block"):
+        block_idx = 0
+        for block in tqdm(blocks, desc="Saving input blocks feature maps"):
+            if not opt.save_all_features and block_idx < 4:
                 block_idx += 1
+                continue
+            if "ResBlock" in str(type(block[0])):
+                if opt.save_all_features or block_idx == 4:
+                    save_feature_map(block[0].in_layers_features, f"{feature_type}_{block_idx}_in_layers_features_time_{i}")
+                    save_feature_map(block[0].out_layers_features, f"{feature_type}_{block_idx}_out_layers_features_time_{i}")
+            if len(block) > 1 and "SpatialTransformer" in str(type(block[1])):
+                save_feature_map(block[1].transformer_blocks[0].attn1.k, f"{feature_type}_{block_idx}_self_attn_k_time_{i}")
+                save_feature_map(block[1].transformer_blocks[0].attn1.q, f"{feature_type}_{block_idx}_self_attn_q_time_{i}")
+            block_idx += 1
 
-        def save_feature_maps_callback(i):
-            if opt.save_all_features:
-                for b in unet_model.middle_block:
-                    if "ResBlock" in str(type(b)):
-                        save_feature_map(b.in_layers_features, f"middle_in_layers_features_time_{i}")
-                        save_feature_map(b.in_layers_features, f"middle_in_layers_features_time_{i}")
+    def save_feature_maps_callback(i):
+        if opt.save_all_features:
+            for b in unet_model.middle_block:
+                if "ResBlock" in str(type(b)):
+                    save_feature_map(b.in_layers_features, f"middle_in_layers_features_time_{i}")
+                    save_feature_map(b.out_layers_features, f"middle_out_layers_features_time_{i}")
 
-                # save_feature_maps(unet_model.middle_block, i, "middle_block")
-                save_feature_maps(unet_model.input_blocks, i, "input_block")
-            save_feature_maps(unet_model.output_blocks , i, "output_block")
+            # save_feature_maps(unet_model.middle_block, i, "middle_block")
+            save_feature_maps(unet_model.input_blocks, i, "input_block")
+        save_feature_maps(unet_model.output_blocks , i, "output_block")
 
-        def save_feature_map(feature_map, filename):
-            save_path = os.path.join(feature_maps_path, f"{filename}.pt")
-            torch.save(feature_map, save_path)
+    def save_feature_map(feature_map, filename):
+        save_path = os.path.join(feature_maps_path, f"{filename}.pt")
+        torch.save(feature_map, save_path)
 
-        assert exp_config.config.prompt is not None
-        prompts = [exp_config.config.prompt]
+    assert exp_config.config.prompt is not None
+    prompts = [exp_config.config.prompt]
 
-        precision_scope = autocast if opt.precision=="autocast" else nullcontext
-        with torch.no_grad():
-            with precision_scope("cuda"):
-                with model.ema_scope():
-                    uc = model.get_learned_conditioning([""])
-                    if isinstance(prompts, tuple):
-                        prompts = list(prompts)
-                    c = model.get_learned_conditioning(prompts)
-                    shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
+    precision_scope = autocast if opt.precision=="autocast" else nullcontext
+    with torch.no_grad():
+        with precision_scope("cuda"):
+            with model.ema_scope():
+                uc = model.get_learned_conditioning([""])
+                if isinstance(prompts, tuple):
+                    prompts = list(prompts)
+                c = model.get_learned_conditioning(prompts)
+                shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
 
-                    z_enc = None
-                    if exp_config.config.init_img != '':
-                        assert os.path.isfile(exp_config.config.init_img)
-                        init_image = load_img(exp_config.config.init_img).to(device)
-                        init_latent = model.get_first_stage_encoding(model.encode_first_stage(init_image))
-                        ddim_inversion_steps = 999
-                        z_enc, _ = sampler.encode_ddim(init_latent, num_steps=ddim_inversion_steps, conditioning=c,unconditional_conditioning=uc,unconditional_guidance_scale=exp_config.config.scale)
-                    else:
-                        z_enc = torch.randn([1, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
-                    torch.save(z_enc, f"{outpath}/z_enc.pt")
-                    samples_ddim, _ = sampler.sample(S=exp_config.config.ddim_steps,
-                                    conditioning=c,
-                                    batch_size=1,
-                                    shape=shape,
-                                    verbose=False,
-                                    unconditional_guidance_scale=exp_config.config.scale,
-                                    unconditional_conditioning=uc,
-                                    eta=opt.ddim_eta,
-                                    x_T=z_enc,
-                                    img_callback=ddim_sampler_callback,
-                                    callback_ddim_timesteps=save_feature_timesteps,
-                                    outpath=outpath)
+                z_enc = None
+                if exp_config.config.init_img != '':
+                    assert os.path.isfile(exp_config.config.init_img)
+                    init_image = load_img(exp_config.config.init_img).to(device)
+                    init_latent = model.get_first_stage_encoding(model.encode_first_stage(init_image))
+                    ddim_inversion_steps = 999
+                    z_enc, _ = sampler.encode_ddim(init_latent, num_steps=ddim_inversion_steps, conditioning=c,unconditional_conditioning=uc,unconditional_guidance_scale=exp_config.config.scale)
+                else:
+                    z_enc = torch.randn([1, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
+                torch.save(z_enc, f"{outpath}/z_enc.pt")
+                samples_ddim, _ = sampler.sample(S=exp_config.config.ddim_steps,
+                                conditioning=c,
+                                batch_size=1,
+                                shape=shape,
+                                verbose=False,
+                                unconditional_guidance_scale=exp_config.config.scale,
+                                unconditional_conditioning=uc,
+                                eta=opt.ddim_eta,
+                                x_T=z_enc,
+                                img_callback=ddim_sampler_callback,
+                                callback_ddim_timesteps=save_feature_timesteps,
+                                outpath=outpath)
 
-                    x_samples_ddim = model.decode_first_stage(samples_ddim)
-                    x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
-                    x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
-                    if opt.check_safety:
-                        x_samples_ddim = check_safety(x_samples_ddim)
-                    x_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
+                x_samples_ddim = model.decode_first_stage(samples_ddim)
+                x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+                if opt.check_safety:
+                    x_samples_ddim = check_safety(x_samples_ddim)
+                x_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
 
-                    sample_idx = 0
-                    for x_sample in x_image_torch:
-                        x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                        img = Image.fromarray(x_sample.astype(np.uint8))
-                        img.save(os.path.join(sample_path, f"{sample_idx}.png"))
-                        sample_idx += 1
+                sample_idx = 0
+                for x_sample in x_image_torch:
+                    x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                    img = Image.fromarray(x_sample.astype(np.uint8))
+                    img.save(os.path.join(sample_path, f"{sample_idx}.png"))
+                    sample_idx += 1
 
-        print(f"Sampled images and extracted features saved in: {outpath}")
+    print(f"Sampled images and extracted features saved in: {outpath}")
 
 
 if __name__ == "__main__":
